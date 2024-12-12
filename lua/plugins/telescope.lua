c0b342786caaace83809b5143aafd5eb9f0421a2
Nahum Trinidad Venancio <nahum.trinidadv@gmail.com>
2024-12-12 18:05:09 -0500

    feat(telescope): add vertical and horizontal split

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,15 +26,19 @@ return {
 	config = function()
 		-- [[ Configure Telescope ]]
 		-- See `:help telescope` and `:help telescope.setup()`
+		local actions = require("telescope.actions")
 		require("telescope").setup({
 			-- You can put your default mappings / updates / etc. in here
 			--  All the info you're looking for is in `:help telescope.setup()`
 			--
-			-- defaults = {
-			--   mappings = {
-			--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-			--   },
-			-- },
+			defaults = {
+				mappings = {
+					i = {
+						["<C-v>"] = actions.select_vertical,
+						["<C-h>"] = actions.select_horizontal,
+					},
+				},
+			},
 			-- pickers = {}
 			extensions = {
 				["ui-select"] = {
